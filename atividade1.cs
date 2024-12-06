3e8f02de9f88322063abf5c4bd0fefc2ade34b37
Laiana angel <laianaangel57@gmail.com>
2024-12-06 14:24:47 -0300

    Add files via upload

---
+++ atividade1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Inicializamos a variável
+        int numero = 1;
+
+        // Laço while que executa enquanto 'numero' for menor ou igual a 10
+        while (numero <= 10)
+        {
+            Console.WriteLine(numero);
+            numero++; // Incrementamos a variável
+        }
+    }
+}
